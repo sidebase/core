1ee338c9ebe160483e3151bbd2dcb8d360536989
Zoey <zoeykaiser8@gmail.com>
2022-12-20 17:58:47 +0100

    feat: Replace NaivieUI components with custom elements, no more outside library dependence (#2)
    * Created custom TimelineItem component
    
    * Removed naiveui from dependencies
    
    * Deleted TheWelcome.test.ts file

---
+++ components/TimelineItem.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+defineProps({
+  title: {
+    type: String,
+    required: true
+  },
+  borderColor: {
+    type: String,
+    default: 'border-blue-500'
+  },
+  content: {
+    type: String,
+    default: undefined
+  }
+})
+</script>
+
+<template>
+  <li>
+    <div class="flex flex-start items-center pt-3">
+      <div :class="borderColor" class="-ml-2 mr-3 h-4 w-4 rounded-full border-2 bg-white"></div>
+      <h4 class="mb-[1px] font-semibold text-gray-800">{{ title }}</h4>
+    </div>
+    <div v-if="content" class="ml-[21px]">
+      <p class="text-gray-500 text-sm">{{ content }}</p>
+    </div>
+  </li>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,4 @@
 import { defineNuxtConfig } from 'nuxt/config'
 
 export default defineNuxtConfig({
-  modules: ['@huntersofbook/naive-ui-nuxt']
 })
